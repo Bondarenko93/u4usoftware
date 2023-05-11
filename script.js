cacded500e5a63485f8c06d964d0c76407cc25ad
Sehii <1flatron1993@gmail.com>
2023-05-11 22:03:36 +0300

    update html

--- script.js
+++ script.js
@@ -44,3 +44,17 @@ var swiper = new Swiper(".mySwiper", {
     }
 
 });
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
